41fffa87456229d7f4f2e654e2681b1ae7f396af
kunal266 <kunalm039@gmail.com>
2021-08-24 04:31:23 +0000

    added files

---
+++ bmi.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void main(){
+    float hei = 0;
+    float wei = 0;
+    cin>>"Height in meters: ">>hei;
+    cin>>"Weight in Kg: ">>wei;
+    float bmi = wei/(hei*hei);
+    
+
+}
